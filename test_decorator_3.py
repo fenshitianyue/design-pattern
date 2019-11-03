d8552f31988c813bbef748eac56e144f5e84b397
fenshitianyue <abzanda@163.com>
2019-11-03 10:36:08 +0800

    test decorator

---
+++ test_decorator_3.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python2
+# -*- coding:UTF-8 -*-
+
+from functools import wraps
+
+# 装饰器
+def decorator_one(func):
+    # 包装你想"修饰的函数"的函数
+    @wraps(func)
+    def wrap_the_func():
+        print 'do some work before func()'
+        func()
+        print 'do some work after func()'
+    # 返回经过包装后的函数
+    return wrap_the_func
+
+
+# 用装饰器 decorator_one 装饰函数 raw_func
+@decorator_one
+def raw_func():
+    print 'hello world'
+
+if __name__ == '__main__':
+    # 如上所见，当我们对装饰器做了如上修改后，就达到了2中我们想要的效果
+    print raw_func.__name__
+
