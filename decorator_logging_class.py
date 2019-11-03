08cd14c488510f6e70e2c9cbffc422d0a869857b
fenshitianyue <abzanda@163.com>
2019-11-03 20:32:10 +0800

    add child class

--- decorator_logging_class.py
+++ decorator_logging_class.py
@@ -3,9 +3,10 @@
 
 from functools import wraps
 
-class logit(object):
+class log_it(object):
     def __init__(self, logfile='out.log'):
         self.logfile = logfile
+
     def __call__(self, f):
         @wraps(f)
         def wrapped_func(*args, **kwargs):
@@ -20,7 +21,22 @@ class logit(object):
     def notify(self):
         pass
 
-@logit()
+# log_it 的子类，更具体的执行某类通知日志
+class email_log_it(log_it):
+    """
+    在函数调用后发送email给管理员
+    """
+    def __init__(self, email='admin@some.com', *args, **kwargs):
+        self.email = email
+        super(email_log_it, self).__init(*args, **kwargs)
+
+    def notify(self):
+        """
+        发送一份email给管理员，暂不实现
+        """
+        pass
+
+@log_it()
 def func():
     print 'hello world'
 
