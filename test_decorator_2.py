bcd3f075b3c933719a0e8335b11114e79af91df0
fenshitianyue <abzanda@163.com>
2019-11-03 10:28:05 +0800

    update code

---
+++ test_decorator_2.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python2
+# -*- coding:UTF-8 -*-
+
+# 装饰器
+def decorator_one(func):
+    # 包装你想"修饰的函数"的函数
+    def wrap_the_func():
+        print 'do some work before func()'
+        func()
+        print 'do some work after func()'
+    # 返回经过包装后的函数
+    return wrap_the_func
+
+
+# 用装饰器 decorator_one 装饰函数 raw_func
+@decorator_one
+def raw_func():
+    print 'hello world'
+
+if __name__ == '__main__':
+    # 看似我们执行的是 raw_func() 函数，实际上 raw_func 已经被
+    # decorator_one装饰过了，真正执行的是经过装饰的函数
+    raw_func()
+
